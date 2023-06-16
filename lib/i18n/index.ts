5bb29a1f62edb38cb3491e74e6a1991cc6b76f33
Bebe Jane <bjornthief@gmail.com>
2023-06-16 17:23:59 +0200

    fix non existent slugs

--- lib/i18n/index.ts
+++ lib/i18n/index.ts
@@ -5,7 +5,7 @@ export { locales, defaultLocale }
 export { default as routes } from './routes'
 
 export const pageSlugs = (id: SectionId, slugs?: PageSlug[]): PageSlug[] => {
-  return locales.map((locale) => ({
+  return locales.filter(l => slugs?.find((s) => s.locale === l && s.value)).map((locale) => ({
     locale: locale as SiteLocale,
     value: `/${routes[id][locale]}${slugs ? `/${slugs.find((s) => s.locale === locale).value}` : ''}`
   }))
