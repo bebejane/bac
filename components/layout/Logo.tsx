11e7729c16bbbe44f61328584724d25ddd2cfe4c
Bebe Jane <bjornthief@gmail.com>
2023-09-01 15:21:17 +0200

    font random

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -11,23 +11,26 @@ export default function Logo({ }: LayoutProps) {
 
 	const { isHome } = usePage()
 
+	const fonts = [s.f1, s.f2, s.f3, s.f4].sort(() => Math.random() - 0.5 > 0 ? 1 : -1)
+
+
 	return (
 		<>
 			<Link href="/">
 				<header className={s.logo}>
 					<h1 className="logo">
 						{'BALTIC '.split('').map((c, i) =>
-							<span key={i} className={cn(isHome && s.home, s.logofont1)}>
+							<span key={i} className={cn(isHome && s.home, fonts[0])}>
 								{c}
 							</span>
 						)}
 						{'ART '.split('').map((c, i) =>
-							<span key={i} className={cn(isHome && s.home, s.logofont2)}>
+							<span key={i} className={cn(isHome && s.home, fonts[1])}>
 								{c}
 							</span>
 						)}
 						{'CENTER '.split('').map((c, i) =>
-							<span key={i} className={cn(isHome && s.home, s.logofont3)}>
+							<span key={i} className={cn(isHome && s.home, fonts[3])}>
 								{c}
 							</span>
 						)}
