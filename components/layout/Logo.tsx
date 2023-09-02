2685842359a27aea537e7fc8ed79e69af9f27627
Bebe Jane <bjornthief@gmail.com>
2023-09-02 12:41:40 +0200

    fix

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -3,14 +3,21 @@ import cn from 'classnames'
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { usePage } from '/lib/context/page'
+import { useRouter } from 'next/router'
+import { is } from 'date-fns/locale'
 export type LayoutProps = {
 
 }
 
 export default function Logo({ }: LayoutProps) {
 
+	const router = useRouter()
 	const { isHome } = usePage()
-	const fonts = [s.f1, s.f2, s.f3, s.f4].sort(() => Math.random() - 0.5 > 0 ? 1 : -1)
+	const [fonts, setFonts] = useState<string[]>([])
+
+	useEffect(() => {
+		setFonts([s.f1, s.f2, s.f3, s.f4].sort(() => Math.random() - 0.5 > 0 ? 1 : -1))
+	}, [router.asPath])
 
 	return (
 		<>
