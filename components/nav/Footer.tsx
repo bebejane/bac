011c4a1367d332909f190671b46546fd8273465c
konst-teknik <information-konst-teknik.se>
2023-09-04 09:59:07 +0200

    Fix

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -16,10 +16,14 @@ export default function Footer({ contact: { email, instagram, mailingAddress } }
 
 	return (
 		<footer className={cn(s.footer)} id="footer">
-			<span>Baltic Art Center</span>
-			<span>{mailingAddress}</span>
-			<span><a href={`mailto:${email}`}>{email}</a></span>
-			<span><a href={instagram}>Instagram</a></span>
+			<div>
+				<span>Baltic Art Center</span>
+				<span>{mailingAddress}</span>
+				<span><a href={`mailto:${email}`}>{email}</a></span>
+			</div>
+			<div>
+				<span><a href={instagram}>Instagram</a></span>
+			</div>
 		</footer>
 	)
 }
