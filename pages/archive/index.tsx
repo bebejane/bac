7f7ba81871c923c6558be81880e1b55f3cd0daf2
Bebe Jane <bjornthief@gmail.com>
2023-09-04 14:44:09 +0200

    fix

--- pages/archive/index.tsx
+++ pages/archive/index.tsx
@@ -32,34 +32,6 @@ export default function Archive({ archives, archiveIntro: { title, text }, archi
 	}, [] as ArchivesByYear).sort((a, b) => a.year < b.year ? 1 : -1);
 
 	return (
-<<<<<<< HEAD
-		<div className={s.container}>
-			<Article
-				id={'archive'}
-				title={title}
-				intro={text}
-				className={s.intro}
-			>
-				<CardContainer>
-					{archivesByYear.map(({ archives, year }, i) => {
-						return (
-							<React.Fragment key={i}>
-								{archives.map(({ title, slug, _createdAt }, idx) =>
-									<Card key={idx}>
-										<Thumbnail
-											typeTitle={idx === 0 ? year.toString() : null}
-											title={title}
-											slug={`/archive/${slug}`}
-										/>
-									</Card>
-								)}
-							</React.Fragment>
-						)
-					})}
-				</CardContainer>
-			</Article>
-		</div>
-=======
 		<Article
 			id={'archive'}
 			title={title}
@@ -68,7 +40,7 @@ export default function Archive({ archives, archiveIntro: { title, text }, archi
 			{archivesByYear.map(({ archives, year }, i) => {
 				return (
 					<CardContainer>
-						<React.Fragment key={year}>
+						<React.Fragment key={i}>
 							{archives.map(({ title, slug, _createdAt }, idx) =>
 								<Card key={idx}>
 									<Thumbnail
@@ -84,11 +56,9 @@ export default function Archive({ archives, archiveIntro: { title, text }, archi
 			})}
 
 		</Article>
->>>>>>> 143c75bda904b22c0b0e8902953caa06cffe6b8b
 
 	);
 }
-
 
 export const getStaticProps = withGlobalProps({ queries: [ArchiveIntroDocument] }, async ({ props, revalidate, context }: any) => {
 
