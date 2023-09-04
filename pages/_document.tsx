697d135db41b3c88d50446d4ba61790b673dd06f
Bebe Jane <bjornthief@gmail.com>
2023-09-04 12:03:47 +0200

    fix archive bg color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
+
   render() {
+    const { page }: { page: PageProps } = this.props?.__NEXT_DATA__?.props?.pageProps || {};
 
     return (
       <Html>
         <Head />
-        <body >
+        <body style={{ background: page.section === 'archive' ? 'var(--archive)' : 'var(--background)' }}>
           <Main />
           <NextScript />
         </body>
