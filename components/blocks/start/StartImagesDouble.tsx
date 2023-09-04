7c7bc12bd52e97b235166ec53b53d3e54d591da7
Bebe Jane <bjornthief@gmail.com>
2023-09-04 15:24:15 +0200

    lines

---
+++ components/blocks/start/StartImagesDouble.tsx
@@ -0,0 +1,42 @@
+import s from './StartImagesDouble.module.scss'
+import React from 'react'
+import { DatoMarkdown as Markdown } from 'dato-nextjs-utils/components'
+import { DatoLink } from '/components'
+import { Image } from 'react-datocms/image'
+
+export type Props = { data: StartImagesDoubleRecord, onClick: Function }
+
+export default function StartImagesDouble({ data: { images } }: Props) {
+
+	return (
+		<section className={s.images}>
+			{images.map(({ image, section, text, title, link }, idx) =>
+				<div className={s.block}>
+					<div className={s.left}>
+						<h2 className={s.section}>
+							{section}
+						</h2>
+						<DatoLink link={link}>
+							<h1>{title}</h1>
+							<Markdown className="mid">{text}</Markdown>
+						</DatoLink>
+					</div>
+					<DatoLink link={link} className={s.right}>
+						<figure>
+							{image &&
+								<>
+									<div className={s.cover}></div>
+									<Image
+										data={image.responsiveImage}
+										className={s.image}
+										pictureClassName={s.picture}
+									/>
+								</>
+							}
+						</figure>
+					</DatoLink >
+				</div >
+			)}
+		</section >
+	)
+}
